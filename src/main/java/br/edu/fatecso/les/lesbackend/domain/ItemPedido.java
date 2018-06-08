363e87216f4c8dddf85b64b15ffeefd004797989
Maylon Magri <maylonmagri@gmail.com>
2018-06-08 14:06:04 -0300

    Inclusao do ItemPedido e seu repositório

---
+++ src/main/java/br/edu/fatecso/les/lesbackend/domain/ItemPedido.java
@@ -0,0 +1,67 @@
+package br.edu.fatecso.les.lesbackend.domain;
+
+import java.io.Serializable;
+
+public class ItemPedido implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Double quantidade;
+	private Produto produto;
+	
+	public ItemPedido() {
+		
+	}
+
+	public ItemPedido(Double quantidade, Produto produto) {
+		super();
+		this.quantidade = quantidade;
+		this.produto = produto;
+	}
+
+	public Double getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Double quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((produto == null) ? 0 : produto.hashCode());
+		result = prime * result + ((quantidade == null) ? 0 : quantidade.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPedido other = (ItemPedido) obj;
+		if (produto == null) {
+			if (other.produto != null)
+				return false;
+		} else if (!produto.equals(other.produto))
+			return false;
+		if (quantidade == null) {
+			if (other.quantidade != null)
+				return false;
+		} else if (!quantidade.equals(other.quantidade))
+			return false;
+		return true;
+	}
+}
---
+++ src/main/java/br/edu/fatecso/les/lesbackend/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package br.edu.fatecso.les.lesbackend.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.fatecso.les.lesbackend.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends MongoRepository<ItemPedido, Double> {
+
+}
