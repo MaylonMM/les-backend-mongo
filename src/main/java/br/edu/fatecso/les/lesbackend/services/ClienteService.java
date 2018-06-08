d27a335fb06fc00d8edea5047a39d0b88a5c3c87
Maylon Magri <maylonmagri@gmail.com>
2018-06-08 11:34:31 -0300

    Inserindo Cliente com POST

--- src/main/java/br/edu/fatecso/les/lesbackend/services/ClienteService.java
+++ src/main/java/br/edu/fatecso/les/lesbackend/services/ClienteService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.edu.fatecso.les.lesbackend.domain.Cliente;
+import br.edu.fatecso.les.lesbackend.dto.ClienteDTO;
 import br.edu.fatecso.les.lesbackend.repositories.ClienteRepository;
 import br.edu.fatecso.les.lesbackend.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class ClienteService {
 	public Cliente findById(String id) {
 		Optional<Cliente> cliente = repo.findById(id);
 		return cliente.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public Cliente insert(Cliente obj) {
+		return repo.insert(obj);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), objDto.getTelefone());
 	}
 }
