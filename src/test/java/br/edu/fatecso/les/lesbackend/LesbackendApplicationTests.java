987502a869abfabb4e64dcc7b122cca8cae3a008
Maylon Magri <maylonmagri@gmail.com>
2018-06-08 09:55:42 -0300

    Projeto Criado

---
+++ src/test/java/br/edu/fatecso/les/lesbackend/LesbackendApplicationTests.java
@@ -0,0 +1,16 @@
+package br.edu.fatecso.les.lesbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LesbackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
